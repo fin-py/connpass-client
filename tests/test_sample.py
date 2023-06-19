c7b3ca58ce354bba55590e5fee4cc8363e298b1f
driller <eleshis@gmail.com>
2023-06-19 13:09:38 +0900

    test: add test #1

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -6,3 +6,12 @@ def test_results():
     assert data["results_returned"] == 1
     assert data["results_available"] == 1
     assert data["results_start"] == 1
+
+
+def test_data_keys():
+    data = ConnpassClient().get(event_id="266898")
+    res = set(data.keys())
+
+    assert res == set(
+        ["results_start", "results_returned", "results_available", "events"]
+    )
