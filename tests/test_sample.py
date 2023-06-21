8505fab8877d77d8a4c85513bd5fc5ccc1325bda
kuchida <kuchida@gmail.com>
2023-06-21 20:09:51 +0900

    add test for exercise2

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -14,3 +14,7 @@ def test_data_keys():
     assert res == set(
         ["results_start", "results_returned", "results_available", "events"]
     )
+
+def test_results_returned():
+   an_event_data = ConnpassClient().get(event_id="266898")
+   assert an_event_data["results_returned"] == len(an_event_data["events"])
