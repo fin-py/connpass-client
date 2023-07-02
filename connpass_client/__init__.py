147a44c42cd2d0eabd989cdd54d60619f8ff92b9
driller <eleshis@gmail.com>
2023-07-02 11:12:53 +0900

    fix: data types arguments

--- connpass_client/__init__.py
+++ connpass_client/__init__.py
@@ -12,7 +12,7 @@ app = typer.Typer()
 @app.command()
 def main(
     event_id: Annotated[
-        str, typer.Option(help="イベント毎に割り当てられた番号で検索します。複数指定可能です")
+        int, typer.Option(help="イベント毎に割り当てられた番号で検索します。複数指定可能です")
     ] = None,
     keyword: Annotated[
         str, typer.Option(help="イベントのタイトル、キャッチ、概要、住所をAND条件部分一致で検索します。複数指定可能です")
@@ -20,8 +20,8 @@ def main(
     keyword_or: Annotated[
         str, typer.Option(help="イベントのタイトル、キャッチ、概要、住所をOR条件部分一致で検索します。複数指定可能です")
     ] = None,
-    ym: Annotated[str, typer.Option(help="指定した年月に開催されているイベントを検索します。複数指定可能です")] = None,
-    ymd: Annotated[str, typer.Option(help="指定した年月日に開催されているイベントを検索します。複数指定可能です")] = None,
+    ym: Annotated[int, typer.Option(help="指定した年月に開催されているイベントを検索します。複数指定可能です")] = None,
+    ymd: Annotated[int, typer.Option(help="指定した年月日に開催されているイベントを検索します。複数指定可能です")] = None,
     nickname: Annotated[
         str, typer.Option(help="指定したニックネームのユーザが参加しているイベントを検索します。複数指定可能です")
     ] = None,
@@ -29,11 +29,11 @@ def main(
         str, typer.Option(help="指定したニックネームのユーザが管理しているイベントを検索します。複数指定可能です")
     ] = None,
     series_id: Annotated[
-        str, typer.Option(help="グループ 毎に割り当てられた番号で、ひもづいたイベントを検索します。複数指定可能です")
+        int, typer.Option(help="グループ 毎に割り当てられた番号で、ひもづいたイベントを検索します。複数指定可能です")
     ] = None,
-    start: Annotated[str, typer.Option(help="検索結果の何件目から出力するかを指定します")] = None,
-    order: Annotated[str, typer.Option(help="検索結果の表示順を、更新日時順、開催日時順、新着順で指定します")] = None,
-    count: Annotated[str, typer.Option(help="検索結果の最大出力データ数を指定します")] = None,
+    start: Annotated[int, typer.Option(help="検索結果の何件目から出力するかを指定します")] = None,
+    order: Annotated[int, typer.Option(help="検索結果の表示順を、更新日時順、開催日時順、新着順で指定します")] = None,
+    count: Annotated[int, typer.Option(help="検索結果の最大出力データ数を指定します")] = None,
     format: Annotated[str, typer.Option(help="レスポンスの形式を指定します")] = None,
     json: Annotated[str, typer.Option(help="指定したファイルにJSON形式で保存します")] = None,
     csv: Annotated[str, typer.Option(help="指定したファイルにCSV形式で保存します")] = None,
